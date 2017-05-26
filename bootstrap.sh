34d75f8fa49662ddaade4f4fa8631c4fd76e8f7b
Weigang Geng <gengwg@gmail.com>
2017-05-25 21:36:02 -0700

    bind to all interfaces

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,11 @@ yum update -y
 yum groupinstall 'Development Tools' -y
 yum install epel-release -y
 yum install python-pip -y
-yum install vim wget -y
-yum localinstall -y /vagrant/redis-3.2.8-4.x86_64.rpm
+yum install vim wget net-tools -y
+
+yum localinstall -y /vagrant/redis-3.2.8-5.x86_64.rpm
 systemctl start supervisord
+systemctl enable supervisord
 
 # install gem
 yum install ruby-devel libyaml-devel -y
